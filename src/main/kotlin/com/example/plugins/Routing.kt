09aee63d5ff7b274e71cc99cf0106af30ee4c017
paritadey <paritasampa95@gmail.com>
2023-07-08 21:54:24 +0530

    getAllHeroes()

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,12 +1,18 @@
 package com.example.plugins
 
+import com.example.routes.getAllHeroes
 import com.example.routes.root
 import io.ktor.application.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() {
     routing {
         root()
+        getAllHeroes()
+        static("/images"){
+            resources("images")
+        }
     }
 }
--- src/main/kotlin/com/example/plugins/koin.kt
+++ src/main/kotlin/com/example/plugins/koin.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.di.koinModule
 import io.ktor.application.*
 import org.koin.ktor.ext.Koin
 import org.koin.logger.slf4jLogger
@@ -7,5 +8,6 @@ import org.koin.logger.slf4jLogger
 fun Application.configureKoin() {
     install(Koin){
         slf4jLogger()
+        modules(koinModule)
     }
 }
